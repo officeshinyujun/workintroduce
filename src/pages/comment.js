d23a15f00889f1298f3e6223fb37ed4793b22ec2
officeshinyujun <officeshinyujun.com>
2024-08-01 01:22:20 +0900

    asdf

--- src/pages/comment.js
+++ src/pages/comment.js
@@ -1,26 +1,30 @@
 import { db } from "../lib/firebase";
-import { query, orderBy, onSnapshot , collection, addDoc } from "firebase/firestore";
+import { query, orderBy, onSnapshot, collection, addDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
-import "../design/coment.css"
-import {Link} from "react-router-dom";
+import "../design/coment.css";
+import { Link } from "react-router-dom";
 
 function Comment() {
-
-    const [comment, setComment] = useState("");
-    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState(""); // 댓글 텍스트 상태
+    const [name, setName] = useState(""); // 이름 상태
+    const [comments, setComments] = useState([]); // 댓글 목록 상태
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (comment.trim()) {
+        // 댓글과 이름이 모두 입력되어야 댓글을 추가
+        if (comment.trim() && name.trim()) {
             await addDoc(collection(db, "comments"), {
                 text: comment,
+                name: name,
                 createdAt: new Date()
             });
-            setComment("");
+            setComment(""); // 댓글 입력 필드 초기화
+            setName(""); // 이름 입력 필드 초기화
         }
     };
 
     useEffect(() => {
+        // 댓글을 작성일자 기준으로 내림차순 정렬하여 가져오기
         const q = query(collection(db, "comments"), orderBy("createdAt", "desc"));
         const unsubscribe = onSnapshot(q, (snapshot) => {
             const commentsData = snapshot.docs.map((doc) => ({
@@ -34,39 +38,47 @@ function Comment() {
         return () => unsubscribe();
     }, []);
 
-    return(
+    return (
         <div className="comment">
             <Link to="/workintroduce/"
-                style={{
-                    position: "absolute",
-                    top: 10,
-                    left : 10,
-                    textDecoration:"none",
-                    color: "black",
-                }}
-
+                  style={{
+                      position: "absolute",
+                      top: 10,
+                      left: 10,
+                      textDecoration: "none",
+                      color: "black",
+                  }}
             > X </Link>
-            <h1>comment</h1>
-            <div style={{width:'100%', marginBottom:"30px"}}>
+            <h1>댓글</h1>
+            <div style={{ width: '100%', marginBottom: "30px" }}>
                 {comments.map((comment) => (
                     <div key={comment.id} className="comment-list">
-                        <p>{comment.text}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27" fill="none">
+                            <path
+                                d="M21.2777 23.5C21.2777 19.8181 18.293 16.8333 14.6111 16.8333H12.3888C8.70694 16.8333 5.72217 19.8181 5.72217 23.5M17.9444 7.94444C17.9444 10.399 15.9545 12.3889 13.4999 12.3889C11.0453 12.3889 9.0555 10.399 9.0555 7.94444C9.0555 5.48985 11.0453 3.5 13.4999 3.5C15.9545 3.5 17.9444 5.48985 17.9444 7.94444Z"
+                                stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                        <p><strong>{comment.name}</strong> : {comment.text}</p> {/* 이름과 댓글 텍스트 표시 */}
                     </div>
                 ))}
             </div>
             <form onSubmit={handleSubmit} className="comment-form">
                 <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="이름..."
+                />
+                <input
                     value={comment}
                     onChange={(e) => setComment(e.target.value)}
-                    placeholder="Comment..."
-                ></input>
+                    placeholder="댓글..."
+                />
                 <div>
                     <button type="submit">+</button>
                 </div>
             </form>
         </div>
     )
-
 }
 
 export default Comment;
--- src/pages/wxvin.js
+++ src/pages/wxvin.js
@@ -4,6 +4,13 @@ import sideView from "../image/sideView.jpg";
 import Header from "../component/header";
 import "../design/main.css";
 import {Link} from "react-router-dom";
+import wxvin11 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-06 001.png"
+import wxvin12 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-06 002.png"
+import wxvin13 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-06 003.png"
+import wxvin21 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-24 001.png"
+import wxvin22 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-25 002.png"
+import wxvin23 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-25 003.png"
+import wxvin24 from "../image/wxvin/KakaoTalk_Photo_2024-08-01-00-22-38.png"
 
 function MainPage() {
     const [modalOpen, setModalOpen] = useState(false);
@@ -45,20 +52,20 @@ function MainPage() {
         <>
             <Header />
             <div className="main-container">
-                <div className="main-work-box1" onClick={openModal}>
-                    <h1 style={{ color: "white" }}>Benz actrocs</h1>
+                <div className="main-work-box-wxvin1" onClick={openModal}>
+                    <h1 style={{ color: "white" }}>Clash Royal - Spaky</h1>
                 </div>
 
-                <div className="main-work-box" onClick={openModal2}>
-                    <h1 style={{ color: "white" }}>JEEP Wrangler</h1>
+                <div className="main-work-box-wxvin2" onClick={openModal2}>
+                    <h1 style={{color: "white", textAlign:"center"}}>Optimus Frime & Megatron</h1>
                 </div>
 
                 <FsLightbox
                     toggler={firstLightboxController.toggler}
                     sources={[
-                        sideView,
-                        'https://i.imgur.com/3egL60k.jpg',
-                        'https://i.imgur.com/RpzrMR2.jpg'
+                        wxvin11,
+                        wxvin12,
+                        wxvin13
                     ]}
                     slide={firstLightboxController.slide}
                 />
@@ -66,9 +73,10 @@ function MainPage() {
                 <FsLightbox
                     toggler={secondLightboxController.toggler}
                     sources={[
-                        'https://i.imgur.com/3egL60k.jpg',
-                        'https://i.imgur.com/RpzrMR2.jpg',
-                        sideView
+                        wxvin21,
+                        wxvin22,
+                        wxvin23,
+                        wxvin24
                     ]}
                     slide={secondLightboxController.slide}
                 />
@@ -87,8 +95,8 @@ function MainPage() {
             {modalOpen && (
                 <div className="modal-container">
                     <div className="modal-content">
-                        <img src={sideView} alt="Side view" style={{width: "100%"}}/>
-                        <h1>Benz actrocs</h1>
+                        <img src={wxvin11} alt="Side view" style={{width: "100%"}}/>
+                        <h1>Clash Royal - Spaky</h1>
                         <div style={{
                             margin: "20px",
                             justifyContent: "center",
@@ -96,18 +104,12 @@ function MainPage() {
                             alignItems: 'center',
                             flexDirection: "column",
                         }}>
-                            <p>글글글그르그그를르르르르르르그를르그ㅏ르가르가르가륵르</p>
-                            <img src={sideView} alt="Side view" style={{width: "150px", maxHeight: "150px"}}
+                            <p>클래시 로얄의 스파키를 모티브로 만들었습니다. </p>
+                            <img src={wxvin11} alt="Side view" style={{width: "150px", maxHeight: "150px", marginBottom:"20px"}}
                                  onClick={() => openFirstLightboxOnSlide(1)}/>
-                            <p>글글글그르그그를르르르르르르그를르그ㅏ르가르가르가륵르</p>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
+                            <img src={wxvin12} alt="Side view" style={{width: "150px", height: "150px", marginBottom:"20px"}}
                                  onClick={() => openFirstLightboxOnSlide(2)}/>
-                            <p>글글글그르그그를르르르르르르그를르그ㅏ르가르가르가륵르</p>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
-                                 onClick={() => openFirstLightboxOnSlide(3)}/>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
-                                 onClick={() => openFirstLightboxOnSlide(3)}/>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
+                            <img src={wxvin13} alt="Side view" style={{width: "150px", height: "150px"}}
                                  onClick={() => openFirstLightboxOnSlide(3)}/>
                             <p onClick={() => setModalOpen(false)} >X</p>
                         </div>
@@ -118,7 +120,7 @@ function MainPage() {
             {modalOpen2 && (
                 <div className="modal-container">
                     <div className="modal-content">
-                        <img src={sideView} alt="Side view" style={{width: "100%"}}/>
+                        <img src={wxvin21} alt="Side view" style={{width: "100%"}}/>
                         <div
                             style={{
                                 margin: "20px",
@@ -128,20 +130,17 @@ function MainPage() {
                                 flexDirection: "column",
 
                             }}>
-                            <h1>JEEP Wrangler</h1>
-                            <p>글글글그르그그를르르르르르르그를르그ㅏ르가르가르가륵르</p>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
+                            <h1  style ={{textAlign:"center"}}>Optimus Frime & Megatron</h1>
+                            <p>트랜스포머 5 최후의 기사에 나온 옵티머스와 메가트론 입니다</p>
+                            <p style={{color:"gray"}}>These are Optimus and Megatron from Transformers 5 The Last Knight.</p>
+                            <img src={wxvin21} alt="Side view" style={{width: "200px", height: "150px"}}
                                  onClick={() => openSecondLightboxOnSlide(1)}/>
-                            <p>글글글그르그그를르르르르르르그를르그ㅏ르가르가르가륵르</p>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
+                            <img src={wxvin22} alt="Side view" style={{width: "200px", height: "150px"}}
                                  onClick={() => openSecondLightboxOnSlide(2)}/>
-                            <p>글글글그르그그를르르르르르르그를르그ㅏ르가르가르가륵르</p>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
+                            <img src={wxvin23} alt="Side view" style={{width: "200px", height: "150px"}}
                                  onClick={() => openSecondLightboxOnSlide(3)}/>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
-                                 onClick={() => openSecondLightboxOnSlide(3)}/>
-                            <img src={sideView} alt="Side view" style={{width: "150px", height: "150px"}}
-                                 onClick={() => openSecondLightboxOnSlide(3)}/>
+                            <img src={wxvin24} alt="Side view" style={{width: "200px", height: "150px"}}
+                                 onClick={() => openSecondLightboxOnSlide(4)}/>
                             <p onClick={() => setModalOpen2(false)}>X</p>
                         </div>
                     </div>
