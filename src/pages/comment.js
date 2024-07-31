8039c5eb0f206988eb3390d5ab2bc6e3dade2d6f
officeshinyujun <officeshinyujun.com>
2024-07-31 22:39:20 +0900

    asdf

--- src/pages/comment.js
+++ src/pages/comment.js
@@ -2,6 +2,7 @@ import { db } from "../lib/firebase";
 import { query, orderBy, onSnapshot , collection, addDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import "../design/coment.css"
+import {Link} from "react-router-dom";
 
 function Comment() {
 
@@ -35,20 +36,33 @@ function Comment() {
 
     return(
         <div className="comment">
-            <div style={{width:'100%'}}>
+            <Link to="/workintroduce/"
+                style={{
+                    position: "absolute",
+                    top: 10,
+                    left : 10,
+                    textDecoration:"none",
+                    color: "black",
+                }}
+
+            > X </Link>
+            <h1>comment</h1>
+            <div style={{width:'100%', marginBottom:"30px"}}>
                 {comments.map((comment) => (
                     <div key={comment.id} className="comment-list">
                         <p>{comment.text}</p>
                     </div>
                 ))}
             </div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="comment-form">
                 <input
                     value={comment}
                     onChange={(e) => setComment(e.target.value)}
-                    placeholder="Comment"
+                    placeholder="Comment..."
                 ></input>
-                <button type="submit">Submit</button>
+                <div>
+                    <button type="submit">+</button>
+                </div>
             </form>
         </div>
     )
