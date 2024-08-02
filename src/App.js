8d62a48555451189ddb4fbdd324b1e6080f0c4a7
officeshinyujun <officeshinyujun.com>
2024-08-02 15:37:12 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import MainPage from "./pages/main";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes , Switch } from "react-router-dom";
 import WxvinPage from "./pages/wxvin";
 import MangooPage from "./pages/mangoo";
 import HaedamPage from "./pages/haedam";
@@ -8,15 +8,15 @@ import Comment from "./pages/comment";
 
 function App() {
     return (
-        <BrowserRouter >
-            <Routes>
-                <Route path="/workintroduce/" element={<MainPage />} />
+        <Router >
+            <Switch>
+                <Route exact path="/workintroduce/" element={<MainPage />} />
                 <Route path="/workintroduce/wxvin" element={<WxvinPage />} />
                 <Route path="/workintroduce/mangoo" element={<MangooPage />} />
                 <Route path="/workintroduce/haedam" element={<HaedamPage />} />
                 <Route path="/workintroduce/comment" element={<Comment />} />
-            </Routes>
-        </BrowserRouter>
+            </Switch>
+        </Router>
     );
 }
 
