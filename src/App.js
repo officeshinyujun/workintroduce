7746596e0d87026bb4d913d51e62242375266415
officeshinyujun <officeshinyujun.com>
2024-08-02 13:32:25 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import MainPage from "./pages/main";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import WxvinPage from "./pages/wxvin";
 import MangooPage from "./pages/mangoo";
 import HaedamPage from "./pages/haedam";
 import Comment from "./pages/comment";
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/workintroduce/" element={<MainPage/>}/>
-            <Route path="/workintroduce/wxvin" element={<WxvinPage/>}/>
-            <Route path="/workintroduce/mangoo" element={<MangooPage/>}/>
-            <Route path="/workintroduce/haedam" element={<HaedamPage/>}/>
-            <Route path="/workintroduce/comment" element={<Comment/>}/>
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/workintroduce/" element={<MainPage />} />
+                <Route path="/workintroduce/wxvin" element={<WxvinPage />} />
+                <Route path="/workintroduce/mangoo" element={<MangooPage />} />
+                <Route path="/workintroduce/haedam" element={<HaedamPage />} />
+                <Route path="/workintroduce/comment" element={<Comment />} />
+            </Routes>
+        </HashRouter>
+    );
 }
 
 export default App;
